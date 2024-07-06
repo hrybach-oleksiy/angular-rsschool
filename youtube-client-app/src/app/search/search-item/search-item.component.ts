c5ad6fe5406e4b1a3d6bb1a39a39f05d486960cd
Hrybach Oleksiy <a.a.gribach@gmail.com>
2024-07-07 00:13:47 +0300

    feat: add button to the card

--- youtube-client-app/src/app/search/search-item/search-item.component.ts
+++ youtube-client-app/src/app/search/search-item/search-item.component.ts
@@ -3,11 +3,12 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
 import { MatIconModule } from '@angular/material/icon';
 import { SearchItemData } from '../../types/interfaces';
+import { CustomButtonComponent } from '../../components/custom-button/custom-button.component';
 
 @Component({
   selector: 'app-search-item',
   standalone: true,
-  imports: [MatButtonModule, MatCardModule, MatIconModule],
+  imports: [MatButtonModule, MatCardModule, MatIconModule, CustomButtonComponent],
   templateUrl: './search-item.component.html',
   styleUrl: './search-item.component.scss',
 })
