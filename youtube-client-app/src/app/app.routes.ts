0c604c8aca6920876241c4e62b799cfae513d8b4
Hrybach Oleksiy <a.a.gribach@gmail.com>
2024-07-15 23:42:33 +0300

    feat: add 404 page

--- youtube-client-app/src/app/app.routes.ts
+++ youtube-client-app/src/app/app.routes.ts
@@ -2,9 +2,11 @@ import { Routes } from '@angular/router';
 import { LoginComponent } from './auth/pages/login/login.component';
 import { MainComponent } from './youtube/pages/main/main.component';
 import { authGuard } from './auth/guards/auth.guard';
+import { NotFoundComponent } from './core/pages/not-found/not-found.component';
 
 export const routes: Routes = [
   { path: 'login', component: LoginComponent },
   { path: 'main', component: MainComponent, canActivate: [authGuard] },
   { path: '', redirectTo: '/login', pathMatch: 'full' },
+  { path: '**', component: NotFoundComponent },
 ];
---
+++ youtube-client-app/src/app/core/pages/not-found/not-found.component.ts
@@ -0,0 +1,14 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { RouterLink } from '@angular/router';
+
+@Component({
+  selector: 'app-not-found',
+  standalone: true,
+  imports: [MatButtonModule, MatCardModule, RouterLink],
+  templateUrl: './not-found.component.html',
+  styleUrl: './not-found.component.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class NotFoundComponent {}
