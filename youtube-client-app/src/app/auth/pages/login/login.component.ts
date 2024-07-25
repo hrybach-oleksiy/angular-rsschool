68953053b19970c282a0549c7406b1ee3c3fcf20
Hrybach Oleksiy <a.a.gribach@gmail.com>
2024-07-26 00:33:03 +0300

    feat: add email validation

--- youtube-client-app/src/app/auth/pages/login/login.component.ts
+++ youtube-client-app/src/app/auth/pages/login/login.component.ts
@@ -1,6 +1,6 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, inject, signal } from '@angular/core';
 import { Router } from '@angular/router';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, FormBuilder, Validators, ReactiveFormsModule } from '@angular/forms';
 import { MatCardModule } from '@angular/material/card';
 import { MatInputModule } from '@angular/material/input';
 import { MatFormFieldModule } from '@angular/material/form-field';
@@ -10,23 +10,41 @@ import { CustomButtonComponent } from '../../../shared/components/custom-button/
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [MatCardModule, MatFormFieldModule, FormsModule, MatInputModule, CustomButtonComponent],
+  imports: [MatCardModule, MatFormFieldModule, MatInputModule, CustomButtonComponent, ReactiveFormsModule, FormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class LoginComponent {
-  public username: string = '';
-  public password: string = '';
+  private readonly loginService = inject(LoginService);
+  private readonly router = inject(Router);
+  private readonly formBuilder = inject(FormBuilder);
 
-  constructor(
-    private loginService: LoginService,
-    private router: Router,
-  ) {}
+  public errorMessage = signal('');
 
-  public login() {
-    this.loginService.login().subscribe(() => {
-      this.router.navigate(['/main']);
-    });
+  public loginForm = this.formBuilder.group({
+    username: ['', [Validators.required, Validators.email]],
+    password: ['', [Validators.required, Validators.minLength(8)]],
+  });
+
+  public get username() {
+    return this.loginForm.get('username');
+  }
+
+  public updateErrorMessage() {
+    if (this.username?.hasError('required')) {
+      this.errorMessage.set('Please enter a login email');
+    } else if (this.username?.hasError('email')) {
+      this.errorMessage.set('The login email is invalid');
+    } else {
+      this.errorMessage.set('');
+    }
+  }
+
+  public onLogin() {
+    // this.loginService.login().subscribe(() => {
+    //   this.router.navigate(['/main']);
+    // });
+    console.log(this.loginForm.value);
   }
 }
